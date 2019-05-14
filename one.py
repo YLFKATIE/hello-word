1e1162e58d55c73e7730a9cff69b0b88e4ebadc8
YLFKATIE <33250589+YLFKATIE@users.noreply.github.com>
2019-05-14 13:37:53 +0800

    Update one.py

--- one.py
+++ one.py
@@ -1,6 +1,17 @@
 import pandas as pd
 import numpy as py 
 x = [1,2,3]
+from IPython.core.display import display, HTML
+
+jsonstr = df_Ali_RC_Data.to_json(orient='records')
+HTML_TEMPLATE = """<link rel="import" href="https://raw.githubusercontent.com/PAIR-code/facets/master/facets-dist/facets-jupyter.html">
+        <facets-dive id="elem" height="600"></facets-dive>
+        <script>
+          var data = {jsonstr};
+          document.querySelector("#elem").data = data;
+        </script>"""
+html = HTML_TEMPLATE.format(jsonstr=jsonstr)
+display(HTML(html))
 print("hello")
 print("github")
 
